29fe8e69aa02b35eae25d5da1d3e767a68df1808
Divyansh Dheer <divyanshdheer42@gmail.com>
2022-08-16 11:53:55 +0530

    task/first changes in random

--- random.js
+++ random.js
@@ -3,3 +3,6 @@ console.log("first commit")
 console.log("first commit")
 console.log("first commit")
 console.log("first commit")
+
+//added after creating task/first branch
+console.log("task.first commit")
