d008ee881690595bbe3d05e84219eb9adde315e5
apoorvbhati <52211016+apoorvbhati@users.noreply.github.com>
2022-08-16 11:58:33 +0530

    random.js: some console logs added by Apoorv

--- random.js
+++ random.js
@@ -1,8 +1,12 @@
-console.log("first commit")
-console.log("first commit")
-console.log("first commit")
-console.log("first commit")
-console.log("first commit")
+console.log("first commit");
+console.log("first commit");
+console.log("first commit");
+console.log("first commit");
 
 //added after creating task/first branch
-console.log("task.first commit")
+console.log("task.first commit");
+
+// Changes made by Apoorv
+console.log("console.log added by Apoorv");
+console.log("console.log added by Apoorv");
+console.log("console.log added by Apoorv");
